e8f105c35b8d384bcfd42ddc2a5de38e5c2ec8c5
Hector <programadorjr@coorsamexico.com>
2023-06-23 16:10:50 -0600

    new migration

--- database/migrations/2023_06_23_190501_create_user_ubicaciones_table.php
+++ database/migrations/2023_06_23_190501_create_user_ubicaciones_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_ubicaciones', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('ubicacion_id')->constrained('ubicaciones');
             $table->timestamps();
         });
     }
