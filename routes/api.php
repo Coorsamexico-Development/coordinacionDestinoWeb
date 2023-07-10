39ec4d941834e36b1a5bd135f114702f804f63c5
Hector <programadorjr@coorsamexico.com>
2023-07-10 12:04:24 -0600

    ruta valores

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AutenticatheController;
 use App\Http\Controllers\CampoController;
 use App\Http\Controllers\ConfirmacionDtController;
+use App\Http\Controllers\ValorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
@@ -38,3 +39,5 @@ Route::get('/dtsApi',[ConfirmacionDtController::class, 'indexApi'])->name('dtsAp
 Route::post('/sanctum/token', [AutenticatheController::class, 'login']);
 //Consultar campos
 Route::get('/camposApi',[CampoController::class, 'indexApi']);
+//Guardar campos con valores en valores
+Route::get('/valores', [ValorController::class, 'valoresApi']);
