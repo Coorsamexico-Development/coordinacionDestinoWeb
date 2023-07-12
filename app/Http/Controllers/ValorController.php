83dd25a7e078bd123d1eae6447510e3278cf504e
Hector <programadorjr@coorsamexico.com>
2023-07-12 13:26:13 -0600

    conteo

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -66,25 +66,11 @@ class ValorController extends Controller
 
     public function valoresApi (Request $request) 
     {
-      $json = [];
       if($request['tipo'] == 'guardar' )
       {
           //Si es guardado envia los datos pero no cambie el status
           //Se recorren los datos y se extraen los campos, al recorrer el ciclo, se insertaran en la BD
-          for ($i=0; $i < count($request['data']) ; $i++)
-          { 
-             $campo_valor = $request['data'][$i];
-             $dt_campo_exist = DtCampoValor::select(
-                'dt_campo_valors.*'
-             )->where('dt_id','=', $request['dt'])
-             ->where('campo_id','=', $campo_valor->campo_id)
-             ->first();
-
-             array_push($json, $dt_campo_exist);
-            
-          }
-
-          return $json;
+          return count($request['data']);
       }
 
       //evidencias bd catalogos, 
