4ce1f1b80734e17bef3060dab02413bca55e93c2
Hector <programadorjr@coorsamexico.com>
2023-07-14 15:50:28 -0600

    prueba cambio de status

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ConfirmacionDt;
 use App\Models\DtCampoValor;
+use App\Models\StatusDt;
 use App\Models\Valor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -168,8 +170,111 @@ class ValorController extends Controller
       if($request['tipo'] == 'siguiente' )
       {
         //Guarda todo y cambia status
+          //Si es guardado envia los datos pero no cambie el status
+          //Se recorren los datos y se extraen los campos, al recorrer el ciclo, se insertaran en la BD
+          for ($i=0; $i < count($data) ; $i++) 
+          { 
+            $campo = $data[$i]; //rescatamos el valor
 
+            $dt_campo = DtCampoValor::select(
+            'dt_campo_valors.*'
+            )
+            ->where('dt_campo_valors.dt_id','=', $request['dt'])
+            ->where('dt_campo_valors.campo_id','=', $campo['campo_id'])
+            ->first();
+
+            if($dt_campo == null)//sino lo encuentra lo creara
+            {
+               $dt_campo = DtCampoValor::create(
+                [
+                   'dt_id' => $request['dt'],
+                   'campo_id' => $campo['campo_id']
+                ]);
+
+                //Hay que encontrar todos los valores anteriores para desactivarlos
+                //y crear uno nuevo
+                $valorADesactivar = Valor::where('valors.dt_campo_valor_id','=',$dt_campo['id'])
+                ->update(['activo' => 0]);
+                //Crea nuevo valor en la tabla de valores
+                $newValor = Valor::create([
+                    'valor' => $campo['value'],
+                    'dt_campo_valor_id' => $dt_campo->id,
+                    'user_id' => $request['usuario']
+                ]);
+                   
+            }
+          }
+
+          //Recorrido para fotos
+          //return $fotos['fotos']['fotos'];
+          $campo_foto = $fotos['campo_id'];
+          $dt_campo_foto = DtCampoValor::select(
+            'dt_campo_valors.*'
+            )
+            ->where('dt_campo_valors.dt_id','=', $request['dt'])
+            ->where('dt_campo_valors.campo_id','=', $campo_foto)
+            ->first();
+         
+         if($dt_campo_foto == null)
+         {
+            $dt_campo_foto = DtCampoValor::create(
+                [
+                   'dt_id' => $request['dt'],
+                   'campo_id' => $campo_foto
+                ]);
+
+           //RECORREMOS las fotos para insercion
+           for ($x=0; $x < count($fotos['fotos']['fotos']) ; $x++) 
+           { 
+              $foto = $fotos['fotos']['fotos'][$x];
+              $valorADesactivar = Valor::where('valors.dt_campo_valor_id','=',$dt_campo_foto['id'])
+              ->update(['activo' => 0]);
+              //Crea nuevo valor en la tabla de valores
+               //Guardar en storage de Google
+               $rutaFoto = $foto['photo']->storeAs('evidencias', $foto['photo'], 'gcs');
+               $urlFoto = Storage::disk('gcs')->url($rutaFoto);
+              $newValor = Valor::create([
+                  'valor' => $urlFoto,
+                  'dt_campo_valor_id' => $dt_campo_foto->id,
+                  'user_id' => $request['usuario']
+              ]);
+           }
+         }
+         else
+         {
+           //RECORREMOS las fotos para insercion
+           for ($x=0; $x < count($fotos['fotos']['fotos']) ; $x++) 
+           { 
+              $foto = $fotos['fotos']['fotos'][$x];
+              $valorADesactivar = Valor::where('valors.dt_campo_valor_id','=',$dt_campo_foto['id'])
+              ->update(['activo' => 0]);
+              //Crea nuevo valor en la tabla de valores
+                //Guardar en storage de Google
+              $rutaFoto = $foto['photo']->storeAs('evidencias', $foto['photo'], 'gcs');
+              $urlFoto = Storage::disk('gcs')->url($rutaFoto);
+              $newValor = Valor::create([
+                  'valor' => $urlFoto,
+                  'dt_campo_valor_id' => $dt_campo_foto->id,
+                  'user_id' => $request['usuario']
+               ]);
+           }
+         }
       }
 
+      $confirmacionDt =  ConfirmacionDt::where('dt_id','=',$request['dt'])
+      ->update([
+         'status_id' => 8 //pasa a documentado
+      ]);
+
+      //desactivamos todos los anteriores
+      StatusDt::where('confirmacion_dt_id','=',$confirmacionDt->id)
+      ->update([
+         'activo' => 0
+      ]);
+
+      StatusDt::create([
+        'confirmacion_dt_id' => $confirmacionDt->id,
+        'status_id' => 8 //status de documentacion 
+      ]);
     }
 }
