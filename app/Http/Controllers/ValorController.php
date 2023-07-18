182ff6efb72c0e1bc40c420c98975b2d0b762326
Hector <programadorjr@coorsamexico.com>
2023-07-18 16:32:00 -0600

    prueba again

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -240,14 +240,14 @@ class ValorController extends Controller
               $foto = $fotos['fotos']['fotos'][$i];
               $newValor = Valor::create([
                 'valor' => $foto['base64'],
-                'dt_campo_valor_id' => $dt_campo_foto->id,
+                'dt_campo_valor_id' => $dt_campo_foto['id'],
                 'user_id' => $request['params']['usuario']
               ]);
            }
         }
         else
         {
-            $valorADesactivar = Valor::where('valors.dt_campo_valor_id','=',$dt_campo_foto->id)
+            $valorADesactivar = Valor::where('valors.dt_campo_valor_id','=',$dt_campo_foto['id'])
                 ->update(['activo' => 0]);
 
             for ($i=0; $i < count($fotos['fotos']['fotos']) ; $i++) 
@@ -255,7 +255,7 @@ class ValorController extends Controller
                $foto = $fotos['fotos']['fotos'][$i];
                $newValor = Valor::create([
                 'valor' => $foto['base64'],
-                'dt_campo_valor_id' => $dt_campo_foto->id,
+                'dt_campo_valor_id' => $dt_campo_foto['id'],
                 'user_id' => $request['params']['usuario']
             ]);
             } 
