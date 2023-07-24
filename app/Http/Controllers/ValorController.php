ff821cfd9b0cf21f28ebfc9da10b545d12ad3594
Hector <programadorjr@coorsamexico.com>
2023-07-24 15:19:28 -0600

    check data

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -398,7 +398,7 @@ class ValorController extends Controller
     public function valoresEnrrampe (Request $request)
     {
       //return $request;
-      return base64_decode($request['documento']);
+      return $request;
       Storage::disk('gcs')->put('file.pdf',base64_decode($request['documento']));
     }
 
