ffb940313bbf5ec59757f477fe191f76889e6f8a
Hector <programadorjr@coorsamexico.com>
2023-07-25 11:43:35 -0600

    return ok

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -404,8 +404,6 @@ class ValorController extends Controller
           $file = request('file');
           $nombre_original = $file->getClientOriginalName();
           $ruta_icono = $file->storeAs('docs', $nombre_original, 'gcs');
-
-          return $ruta_icono;
           $urlIcono = Storage::disk('gcs')->url($ruta_icono);
 
           return 'ok';
