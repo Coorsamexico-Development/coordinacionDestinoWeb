5942061f0987d4cf1cf5f43528ba542f0f6f0490
Hector <programadorjr@coorsamexico.com>
2023-07-28 14:04:21 -0600

    incorporacion de firma

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -452,21 +452,14 @@ class ValorController extends Controller
           //Creamoe el documento de verificacion y lo guardamos
           $pdf->loadHTML('
           <html>
-              <head>
-                <title>Confirmacion'.$request['params']['confirmacion'].'</title>
-             </head>
-             '.'
-             <body>
-                 <h1 style="color:red">{{ $title }}</h1>
-                 <p>{{ $date }}</p>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-             </body>
-             </html>
+               <head>
+                 <title>Confirmacion'.$request['params']['confirmacion'].'</title>
+               </head>
+               '.'
+               <body>
+                 <img src="'.$request['params']['firma'].'">
+               </body>
+          </html>
           ');
           
           Storage::disk('gcs') //guardamos en google
