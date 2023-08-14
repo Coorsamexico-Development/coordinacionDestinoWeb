949a253d5c8f51381b099150dc99c780f636be99
Hector <programadorjr@coorsamexico.com>
2023-08-14 12:04:31 -0600

    cf

--- app/Http/Controllers/ConfirmacionDtController.php
+++ app/Http/Controllers/ConfirmacionDtController.php
@@ -272,7 +272,17 @@ class ConfirmacionDtController extends Controller
 
       if($request['fecha'])
       {
-        return $confirmaciones = ConfirmacionDt::select(
+        return $confirmaciones = StatusDt::select('status_dts*')
+        ->join('confirmacion_dts','status_dts.confirmacion_dt_id','confirmacion_dts.id')
+        ->join('dts','confirmacion_dts.dt_id','dts.id')
+        ->join('linea_transportes','confirmacion_dts.linea_transporte_id','linea_transportes.id')
+        ->join('plataformas','confirmacion_dts.plataforma_id','plataformas.id')
+        ->where('confirmacion_dts.status_id','=', $status['id'])
+        ->where('confirmacion_dts.ubicacion_id','=', $ubicacion['id'])
+        ->where('confirmacion_dts.cita','LIKE','%'.$request['fecha'].'%')
+        ->get();
+        
+      /*$confirmaciones = ConfirmacionDt::select(
               'confirmacion_dts.id',
               'confirmacion_dts.confirmacion',
               'confirmacion_dts.pdf',
@@ -286,34 +296,16 @@ class ConfirmacionDtController extends Controller
               )
        ->where('confirmacion_dts.status_id','=', $status['id'])
        ->where('confirmacion_dts.ubicacion_id','=', $ubicacion['id'])
-       //->where('confirmacion_dts.cita','LIKE','%'.$request['fecha'].'%')
+       ->where('confirmacion_dts.cita','LIKE','%'.$request['fecha'].'%')
        ->join('dts','confirmacion_dts.dt_id','dts.id')
        ->join('linea_transportes','confirmacion_dts.linea_transporte_id','linea_transportes.id')
        ->join('plataformas','confirmacion_dts.plataforma_id','plataformas.id')
        ->get();
+       */
       }
       else
       {
-              $hoy = date("Y-m-d");   
-              return $confirmaciones = ConfirmacionDt::select(
-                'confirmacion_dts.id',
-                'confirmacion_dts.confirmacion',
-                'confirmacion_dts.pdf',
-                'confirmacion_dts.cita',
-                'confirmacion_dts.numero_cajas',
-                'confirmacion_dts.pdf',
-                'confirmacion_dts.dt_id as dt_id',
-                'dts.referencia_dt as dt',
-                'linea_transportes.nombre as linea_transporte',
-                'plataformas.nombre as plataforma'
-                )
-         ->where('confirmacion_dts.status_id','=', $status['id'])
-         ->where('confirmacion_dts.ubicacion_id','=', $ubicacion['id'])
-         ->where('confirmacion_dts.cita','LIKE','%'.$hoy.'%')
-         ->join('dts','confirmacion_dts.dt_id','dts.id')
-         ->join('linea_transportes','confirmacion_dts.linea_transporte_id','linea_transportes.id')
-         ->join('plataformas','confirmacion_dts.plataforma_id','plataformas.id')
-         ->get();
+        $hoy = date("Y-m-d");   
       }
 
   }
