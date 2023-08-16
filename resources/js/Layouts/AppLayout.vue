58f9b5160195f79c082178bf008b3ce0a40ab639
Hector <programadorjr@coorsamexico.com>
2023-08-16 09:17:25 -0600

    other

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -133,12 +133,14 @@ const logout = () => {
                                         </button>
 
                                         <span v-else class="inline-flex rounded-md">
-                                            <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50">
-                                                {{ $page.props.auth.user.name }}
-
+                                            <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-transparent border border-transparent rounded-md hover:text-gray-700 focus:outline-none ">
+                                                <p class="text-[#1983db] uppercase">{{ $page.props.auth.user.name }}</p>
+                                                <img class="w-8 ml-8" src="../../assets//img/icon_user.png"  />
+                                                <!--
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                 </svg>
+                                                -->
                                             </button>
                                         </span>
                                     </template>
@@ -162,7 +164,7 @@ const logout = () => {
                                         <!-- Authentication -->
                                         <form @submit.prevent="logout">
                                             <DropdownLink as="button">
-                                                Log Out
+                                                Salir
                                             </DropdownLink>
                                         </form>
                                     </template>
