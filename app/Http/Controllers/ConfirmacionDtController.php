47459f158f03e73042a7edbeb08fdd59c74f700c
Hector <programadorjr@coorsamexico.com>
2023-09-14 17:01:04 -0600

    check save valors

--- app/Http/Controllers/ConfirmacionDtController.php
+++ app/Http/Controllers/ConfirmacionDtController.php
@@ -359,7 +359,6 @@ class ConfirmacionDtController extends Controller
 
   public function valoresLiberacion (Request $request)
   {
-       return $request;
        $confirmacion_Dt = ConfirmacionDt::select('confirmacion_dts.*')
        ->where('confirmacion_dts.confirmacion','=',$request['params']['confirmacion'])
        ->first();
