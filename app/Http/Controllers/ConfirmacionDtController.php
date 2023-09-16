7a43f614f7554c96c0b48af11539edccc0969676
Hector <programadorjr@coorsamexico.com>
2023-09-16 12:19:17 -0600

    ubicacion

--- app/Http/Controllers/ConfirmacionDtController.php
+++ app/Http/Controllers/ConfirmacionDtController.php
@@ -41,7 +41,7 @@ class ConfirmacionDtController extends Controller
         ->join('linea_transportes', 'confirmacion_dts.linea_transporte_id', 'linea_transportes.id')
         ->join('status', 'confirmacion_dts.status_id', 'status.id')
         ->where('status.status_padre','=',$request['status_id'])
-        //->where('confirmacion_dts.ubicacion_id','=',$request['ubicacion_id'])
+        ->where('confirmacion_dts.ubicacion_id','=',$request['ubicacion_id'])
         ->where('confirmacion_dts.plataforma_id','=',$request['plataforma_id']);
 
         if($request->has("busqueda"))
