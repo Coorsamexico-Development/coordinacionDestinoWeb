bc144eeccb206d7ab1ba5ad8f00e6c1082090c50
Hector <programadorjr@coorsamexico.com>
2023-10-09 09:52:52 -0600

    try to save

--- app/Http/Controllers/ValorController.php
+++ app/Http/Controllers/ValorController.php
@@ -333,7 +333,7 @@ class ValorController extends Controller
     {
        //RECORRIDO DE PRUEBA
        $fotos = $request['params']['fotos']; //tenemos el arreglo de fotos
-       $fotoTemp =$fotos[0]['fotos'][0];
+       $fotoTemp =$fotos;
        //return $fotoTemp;
        $rutaImage = $fotoTemp->store('img/fotos', 'gcs');
        $urlImage = Storage::disk('gcs')->url($rutaImage);
