3fcb927c2f3368528c37bae785fc9dfaa9a6eff5
Hector <programadorjr@coorsamexico.com>
2023-10-27 13:39:37 -0600

    doble punto coma

--- app/Http/Controllers/ConfirmacionDtController.php
+++ app/Http/Controllers/ConfirmacionDtController.php
@@ -191,7 +191,6 @@ class ConfirmacionDtController extends Controller
                     'confirmacion_dts.*',
                 )
                 ->where('confirmacion_dts.cerrado','=',0);
-                ;
                 //->where('confirmacion_dts.ubicacion_id','=',$request['ubicacion_id']);
             }]
             )
