51b0b9851ace4e669d2b133d9a32a262f7a34de6
Hector <programadorjr@coorsamexico.com>
2023-10-30 16:28:52 -0600

    check

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Auth;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('confirmacion.{id}', function ( $id) 
+{
+    return  (int) $id;
+});
