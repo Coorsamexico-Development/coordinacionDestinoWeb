9a2e6d3c84dbad0a06a0fe691292177124fbf7df
Hector <programadorjr@coorsamexico.com>
2023-10-30 15:54:54 -0600

    sin paginacion

--- resources/js/Pages/Reportes/Partials/UbicacionDesplegable.vue
+++ resources/js/Pages/Reportes/Partials/UbicacionDesplegable.vue
@@ -165,7 +165,9 @@ const valores = computed(() =>
              <div class="pb-1" v-for="dt in dts.data" :key="dt.id">
                 <DtBlock :dt="dt"  />
              </div>
+             <!--
              <PaginationAxios @loadPage="loadPage($event)" :pagination="dts" />
+             -->
           </div>
         </div>
      </Transition>
