525a1d0cf1f56fabed152c1c6b05377c5d9774c1
Hector <programadorjr@coorsamexico.com>
2023-11-10 15:33:27 -0600

    check

--- app/Http/Controllers/DtController.php
+++ app/Http/Controllers/DtController.php
@@ -42,6 +42,47 @@ class DtController extends Controller
           }
         }
 
+        if ($request->has("fechaInicial") && $request->has("fechaFinal")) 
+        {
+          if($request['fechaInicial'] !== null && $request->has("fechaFinal"))
+          {
+            $añoInicial = $request['fechaInicial']['year'];
+            $mesInicial = $request['fechaInicial']['month']+1;
+            $newFechaInicial = null;
+       
+            if($mesInicial <= 9)
+            {
+             $newFechaInicial = $añoInicial.'-0'.$mesInicial.'-01';
+            }
+            else
+            {
+             $newFechaInicial = $añoInicial.'-'.$mesInicial.'-01';
+            }
+            
+            $añoFinal = $request['fechaFinal']['year'];
+            $mesFinal = $request['fechaFinal']['month']+1;
+            $newFechaFinal = null;
+       
+            if($mesFinal <= 9)
+            {
+             $newFechaFinal = $añoFinal.'-0'.$mesFinal.'-01';
+            }
+            else
+            {
+             $newFechaFinal = $añoFinal.'-'.$mesFinal.'-01';
+            }
+
+            $fechaInicialABuscar = strtr($newFechaInicial, array("'" => "\\'", "%" => "\\%"));
+            $fechaFinalABuscar = strtr($newFechaFinal, array("'" => "\\'", "%" => "\\%"));
+        
+            $viajes->where(function($query) use ($fechaInicialABuscar, $fechaFinalABuscar)
+               {
+                 $query->whereBetween('confirmacion_dts.cita',[ $fechaInicialABuscar, $fechaFinalABuscar]);
+               });
+
+          }
+        }
+
         $viajes->where(function($query)
           {
             $query->where('confirmacion_dts.status_id','=',10)
