f43ddf1e1ea292ad571e6416f92339889a2ea6b1
Hector <programadorjr@coorsamexico.com>
2023-11-13 15:43:22 -0600

    another migration

---
+++ app/Http/Controllers/ConfirmacionFechasPodController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\confirmacionFechasPod;
+use Illuminate\Http\Request;
+
+class ConfirmacionFechasPodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(confirmacionFechasPod $confirmacionFechasPod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(confirmacionFechasPod $confirmacionFechasPod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, confirmacionFechasPod $confirmacionFechasPod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(confirmacionFechasPod $confirmacionFechasPod)
+    {
+        //
+    }
+}
