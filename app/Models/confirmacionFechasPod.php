0aa6592cf17275026485f6e29311d873fe100d74
Hector <programadorjr@coorsamexico.com>
2023-11-14 09:34:56 -0600

    migraciones daltantes

--- app/Models/confirmacionFechasPod.php
+++ app/Models/confirmacionFechasPod.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class confirmacionFechasPod extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'confirmacion_dt_id',
+        'fecha_pod_id',
+        'activo'
+    ];
 }
