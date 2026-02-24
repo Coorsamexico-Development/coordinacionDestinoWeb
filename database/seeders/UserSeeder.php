f06305ba27bdd03b4d7142090a90f7897e7d29fe
Tonatiuh Valladares <tonatiuhvalladares@gmail.com>
2026-02-24 15:41:57 -0600

    UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,17 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        // DB::table('users')->insert([
+        //     'name' => "admin",
+        //     'email' => "admin@coorsamexico.com",
+        //     'password' => Hash::make("c00rs4m3x1c0"),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
         DB::table('users')->insert([
-            'name' => "admin",
-            'email' => "admin@coorsamexico.com",
-            'password' => Hash::make("c00rs4m3x1c0"),
+            'name' => "test",
+            'email' => "test@coorsamexico.com",
+            'password' => Hash::make("t3stc00rs4"),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
